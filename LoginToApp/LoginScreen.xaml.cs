a30821339e023c1306c149ce65791cc9a5a7bef4
Patryk R <rajca.patryk@gmail.com>
2018-08-23 20:35:20 +0100

    I'am added early login window.

---
+++ LoginToApp/LoginScreen.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginToApp
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(tb_Login.Text) || !String.IsNullOrWhiteSpace(pb_Password.Password))
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Enter your login/password on correctly box.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
